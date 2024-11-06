e29a7e63ad4ef6e80926bc49b1b2bb0face2dbda
Jorge Emanoel <eu.jorge.emanoell@gmail.com>
2024-11-05 21:31:57 -0300

    colorscheme

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,17 +1,14 @@
--- local M = {
---   {
---     "catppuccin/nvim",
---     name = "catppuccin",
---     priority = 1000,
---     opts = {
---       transparent_background = true,
---     },
---   },
--- }
---
--- function M.config()
---   vim.cmd.colorscheme "catppuccin-mocha"
--- end
---
-local M = {}
+local M = {
+  "catppuccin/nvim",
+  name = "catppuccin",
+  priority = 1000,
+  opts = {
+    transparent_background = false,
+  },
+}
+
+function M.config()
+  vim.cmd.colorscheme "catppuccin-frappe"
+end
+
 return M
